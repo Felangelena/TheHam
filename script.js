c95639298a4d09fce0740f38be1665e32acbc10b
Olena Oliianenko <felangelena@gmail.com>
2023-05-13 15:18:38 +0200

    feat: add section Services

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 "use strict";
+
+const tab = document.querySelector(".services-tabs");
+const tabs = document.querySelectorAll(".tab-title");
+const contents = document.querySelectorAll(".tab-content");
+
+tab.addEventListener("click", (e) => {
+    removeActive(tabs);
+    removeActive(contents);
+    setNewActive(e);
+});
+
+function removeActive(items) {
+    items.forEach(item => {
+        item.classList.remove("active");
+    });
+}
+
+function setNewActive(e) {
+    const target = e.target.dataset.tab;
+    document.querySelector(`[data-tab="${target}"]`).classList.add("active");
+    document.querySelector(`[data-content="${target}"]`).classList.add("active");
+}
