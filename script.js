64f4c0d9b09da74a645db0feee0b9d89b34c0593
Olena Oliianenko <felangelena@gmail.com>
2023-05-13 18:26:14 +0200

    feat: add section Work

--- script.js
+++ script.js
@@ -1,15 +1,5 @@
 "use strict";
 
-const tab = document.querySelector(".services-tabs");
-const tabs = document.querySelectorAll(".tab-title");
-const contents = document.querySelectorAll(".tab-content");
-
-tab.addEventListener("click", (e) => {
-    removeActive(tabs);
-    removeActive(contents);
-    setNewActive(e);
-});
-
 function removeActive(items) {
     items.forEach(item => {
         item.classList.remove("active");
@@ -19,5 +9,34 @@ function removeActive(items) {
 function setNewActive(e) {
     const target = e.target.dataset.tab;
     document.querySelector(`[data-tab="${target}"]`).classList.add("active");
-    document.querySelector(`[data-content="${target}"]`).classList.add("active");
+    document.querySelectorAll(`[data-content="${target}"]`).forEach(item => {
+        item.classList.add("active");
+    });
 }
+
+const tabList = document.querySelector(".services-tabs");
+const tabs = document.querySelectorAll(".tab-title");
+const contents = document.querySelectorAll(".tab-content");
+
+tabList.addEventListener("click", (e) => {
+    removeActive(tabs);
+    removeActive(contents);
+    setNewActive(e);
+});
+
+const workTabList = document.querySelector(".work-tabs");
+const workTabs = document.querySelectorAll(".work-tab-title");
+const workContents = document.querySelectorAll(".work-content");
+
+workTabList.addEventListener("click", (e) => {
+    removeActive(workTabs);
+    removeActive(workContents);
+    if(e.target.dataset.tab == "All") {
+        e.target.classList.add("active");
+        workContents.forEach(item => {
+            item.classList.add("active");
+        })
+    } else {
+        setNewActive(e);
+    }
+});
